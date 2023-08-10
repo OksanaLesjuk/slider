931edaf16b99593ffedc6348d2fbbbc1affac0e3
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-08-10 22:21:24 +0300

    update

--- js/slide.js
+++ js/slide.js
@@ -1,10 +1,9 @@
 
 $('.variable-width').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true
-  });
-       
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true
+});
